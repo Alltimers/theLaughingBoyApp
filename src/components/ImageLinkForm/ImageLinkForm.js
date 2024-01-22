77f5b1187c5b5bc2556f1804d5a91d6625bfd20e
alltimers <pablowildmann@gmail.com>
2024-01-22 14:50:24 -0300

    update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
         <div className='form center pa4 br3 ba red shadow-5'>
           <input className='f4 pa2 w-70 bg-black center' type='text' onChange={onInputChange}/>
           <button
-            className='w-30 grow hover-black f4 link ph3 pv2 dib moon-gray bg-red'
+            className='w-30 grow hover-black f5 link ph3 pv2 dib moon-gray bg-red'
             onClick={onButtonSubmit}
           >Detect</button>
         </div>
