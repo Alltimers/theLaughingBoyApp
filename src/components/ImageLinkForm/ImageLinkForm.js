012b9b77d6f6dd4ee7849ab2f0948735508a220a
alltimers <pablowildmann@gmail.com>
2024-02-10 19:17:13 -0300

    update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,9 +4,11 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
+    <div className="center card br3 ba b--black-10 mv2 w-100 w-50-m w-25-l mw6 shadow-5 ">
       <p className='submit f5'>
         {'Please submit a URL image below'}
       </p>
+      </div>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 bg-black center' type='text' onChange={onInputChange}/>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,17 +3,19 @@ import rank from './rank.css';
 
 const Rank = ({ name, entries }) => {
   return (
-    <div>
-    <p className='quote'>"And I have one of those very loud, stupid laughs.<br /> 
-      I mean if I ever sat behind myself in a movie or something,<br /> 
-      I’d probably lean over and tell myself to please shut up."<br />
-      <span className='author'> Holden Caulfield in The catcher in the rye, by J.D. Salinger.</span>
-    </p>
-      <div className='counter f3'>
-        {`Hi ${name}, your current hack count is:`}
-      </div>
-      <div className='entry f1'>
-        {entries}
+    <div className="center">
+      <div className="card br3 ba b--black-10 mv4 w-100 w-50-m w-100-l mw6 shadow-5 ">
+      <p className='quote'>"And I have one of those very loud, stupid laughs.<br /> 
+        I mean if I ever sat behind myself in a movie or something,<br /> 
+        I’d probably lean over and tell myself to please shut up."<br />
+        <span className='author'> Holden Caulfield in The catcher in the rye, by J.D. Salinger.</span>
+      </p>
+        <div className='counter f3'>
+          {`Hi ${name}, your current hack count is:`}
+        </div>
+        <div className='entry f1'>
+          {entries}
+        </div>
       </div>
     </div>
   );
