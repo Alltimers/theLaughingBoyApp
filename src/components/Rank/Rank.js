64f23db9e87035b51290cb571bccb18b0c958e61
alltimers <pablowildmann@gmail.com>
2024-02-12 12:52:48 -0300

    update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -5,9 +5,9 @@ const Rank = ({ name, entries }) => {
   return (
     <div className="center">
       <div className="card br3 ba b--black-10 w-100 w-50-m w-100-l mw6 shadow-5 ">
-      <p className='quote'>"And I have one of those very loud, stupid laughs.<br /> 
+      <p className='quotes'>And I have one of those very loud, stupid laughs.<br /> 
         I mean if I ever sat behind myself in a movie or something,<br /> 
-        I’d probably lean over and tell myself to please shut up."<br />
+        I’d probably lean over and tell myself to please shut up.<br />
         <span className='author'> Holden Caulfield in The catcher in the rye, by J.D. Salinger.</span>
       </p>
         <div className='counter f3'>
